28e53b4e56795079362d0ec00ec98cc99d1c72d9
Nayan Swarnkar <swarnkarnayan13@gmail.com>
2025-11-02 00:57:14 +0530

    add vercel configuration and refactor vite config for build settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    cors: true,
-    proxy: {
-      // Optionally, you can add a proxy if needed - not required with the CORS changes
-      // '/api': {
-      //   target: 'http://localhost:5001',
-      //   changeOrigin: true,
-      //   secure: false
-      // }
-    },
-  },
+	plugins: [react()],
+	build: {
+		outDir: 'dist',
+		sourcemap: false
+	},
+	server: {
+		port: 5173,
+		cors: true,
+		proxy: {
+			// Optionally, you can add a proxy if needed - not required with the CORS changes
+			// '/api': {
+			//   target: 'http://localhost:5001',
+			//   changeOrigin: true,
+			//   secure: false
+			// }
+		}
+	}
 });
